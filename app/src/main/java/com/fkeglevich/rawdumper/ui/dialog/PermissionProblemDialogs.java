3e171a620eb8f679b8f44edcf9dc8c0ffc67411e
fkeglevich <fkeglevich@gmail.com>
2017-08-14 20:52:03 -0300

    Update some UI code

---
+++ app/src/main/java/com/fkeglevich/rawdumper/ui/dialog/PermissionProblemDialogs.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.ui.dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fkeglevich.rawdumper.R;
+import com.fkeglevich.rawdumper.ui.UiUtils;
+
+/**
+ * Created by Flávio Keglevich on 13/08/2017.
+ * TODO: Add a class header comment!
+ */
+
+public class PermissionProblemDialogs
+{
+    private final AlertDialog needsPermissionDialog;
+    private final AlertDialog needsRootAccessDialog;
+
+    public PermissionProblemDialogs(Context context)
+    {
+        needsPermissionDialog = buildTerminatingDialog(context, context.getResources().getString(R.string.permission_error));
+        needsRootAccessDialog = buildTerminatingDialog(context, context.getResources().getString(R.string.root_access_error));
+    }
+
+    public void showNeedsPermissionDialog(AppCompatActivity activity)
+    {
+        UiUtils.showDialogInImmersiveMode(needsPermissionDialog, activity);
+    }
+
+    public void showNeedsRootAccessDialog(AppCompatActivity activity)
+    {
+        UiUtils.showDialogInImmersiveMode(needsRootAccessDialog, activity);
+    }
+
+    private AlertDialog buildTerminatingDialog(Context context, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setMessage(message);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(
+                context.getResources().getString(R.string.terminating_dialog_button),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                });
+
+        return builder.create();
+    }
+}
