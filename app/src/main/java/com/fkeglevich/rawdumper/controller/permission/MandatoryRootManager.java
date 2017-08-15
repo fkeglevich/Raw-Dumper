935a0a3b0added03f49b76b8af00cc3932220efe
fkeglevich <fkeglevich@gmail.com>
2017-08-15 04:22:47 -0300

    Update the new permission small library

---
+++ app/src/main/java/com/fkeglevich/rawdumper/controller/permission/MandatoryRootManager.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.controller.permission;
+
+import com.fkeglevich.rawdumper.su.ShellManager;
+
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
+
+/**
+ * Created by Flávio Keglevich on 09/08/2017.
+ * TODO: Add a class header comment!
+ */
+
+class MandatoryRootManager extends MandatoryPermissionManager
+{
+    MandatoryRootManager(IPermissionResultListener listener)
+    {
+        super(listener);
+    }
+
+    @Override
+    protected void allPermissionsWereGranted(boolean hadDialogPrompt)
+    {
+        if (!ShellManager.getInstance().isRunning())
+        {
+            ShellManager.getInstance().open(new Shell.OnCommandResultListener()
+            {
+                @Override
+                public void onCommandResult(int commandCode, int exitCode, List<String> output)
+                {
+                    if (exitCode == Shell.OnCommandResultListener.SHELL_RUNNING)
+                        permissionResultListener.onAllPermissionsGranted(true);
+                    else
+                        permissionResultListener.onMissingRootAccess();
+                }
+            });
+        }
+        else
+        {
+            permissionResultListener.onAllPermissionsGranted(hadDialogPrompt);
+        }
+    }
+}
