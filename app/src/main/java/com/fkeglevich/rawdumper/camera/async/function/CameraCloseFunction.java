aea92b339ab230f77ef8f9410e049f7f25892f2c
Flávio Keglevich <fkeglevich@gmail.com>
2017-10-23 21:07:21 -0200

    Added camera close function for closing the camera asynchronously

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/async/function/CameraCloseFunction.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.camera.async.function;
+
+import com.fkeglevich.rawdumper.async.function.AsyncFunction;
+import com.fkeglevich.rawdumper.camera.async.Closeable;
+import com.fkeglevich.rawdumper.camera.async.TurboCamera;
+import com.fkeglevich.rawdumper.util.Nothing;
+
+import java.util.Objects;
+
+/**
+ * TODO: Add class header
+ * <p>
+ * Created by Flávio Keglevich on 19/10/17.
+ */
+
+public class CameraCloseFunction extends AsyncFunction<TurboCamera, Nothing>
+{
+    @Override
+    protected Nothing call(TurboCamera argument)
+    {
+        ((Closeable) argument).close();
+        return Nothing.NOTHING;
+    }
+}
