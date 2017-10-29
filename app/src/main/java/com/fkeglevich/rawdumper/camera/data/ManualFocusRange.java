bda886ce4e63db902311cc329d1add737ec028bb
Flávio Keglevich <fkeglevich@gmail.com>
2017-10-29 19:16:14 -0200

    Created ManualFocusRange class

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/data/ManualFocusRange.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.camera.data;
+
+/**
+ * TODO: Add class header
+ * <p>
+ * Created by Flávio Keglevich on 29/10/17.
+ */
+
+public class ManualFocusRange
+{
+    private final ManualFocus lower;
+    private final ManualFocus upper;
+
+    //focus_range_values: 5~100,1
+    public static ManualFocusRange parseRange(String range)
+    {
+         String[] bounds = range.split(",")[0].split("~");
+         ManualFocus lower = ManualFocus.create(Integer.parseInt(bounds[0]));
+         ManualFocus upper = ManualFocus.create(Integer.parseInt(bounds[1]));
+         return new ManualFocusRange(lower, upper);
+    }
+
+    private ManualFocusRange(ManualFocus lower, ManualFocus upper)
+    {
+        this.lower = lower;
+        this.upper = upper;
+    }
+
+    public boolean contains(ManualFocus value)
+    {
+        if (value.equals(ManualFocus.DISABLED))
+            return true;
+
+        return value.compareTo(lower) >= 0 && value.compareTo(upper)  <= 0;
+    }
+}
