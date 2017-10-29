2728b2ea0ff5b64541035c4b60bc0a99eb6daa51
Flávio Keglevich <fkeglevich@gmail.com>
2017-10-28 22:43:40 -0200

    Master clean up of MainActivity5

--- app/src/main/java/com/fkeglevich/rawdumper/MainActivity5.java
+++ app/src/main/java/com/fkeglevich/rawdumper/MainActivity5.java
@@ -17,31 +17,13 @@
 package com.fkeglevich.rawdumper;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import com.fkeglevich.rawdumper.activity.ModularActivity;
-import com.fkeglevich.rawdumper.camera.async.CameraManager;
-import com.fkeglevich.rawdumper.camera.async.TurboCamera;
-import com.fkeglevich.rawdumper.camera.feature.WritableFeature;
-import com.fkeglevich.rawdumper.controller.adapter.ButtonController;
-import com.fkeglevich.rawdumper.controller.adapter.DismissibleManagerAdapter;
-import com.fkeglevich.rawdumper.controller.adapter.ToastNotificationController;
-import com.fkeglevich.rawdumper.controller.adapter.WheelViewAdapter;
-import com.fkeglevich.rawdumper.controller.feature.DisplayableFeatureController;
-import com.fkeglevich.rawdumper.controller.feature.FeatureControllerFactory;
-import com.fkeglevich.rawdumper.controller.feature.FeatureControllerManager;
+import com.fkeglevich.rawdumper.controller.feature.CameraLifetimeController;
 import com.fkeglevich.rawdumper.controller.orientation.OrientationModule;
 import com.fkeglevich.rawdumper.controller.permission.MandatoryPermissionModule;
-import com.fkeglevich.rawdumper.ui.CameraPreviewTexture;
 import com.fkeglevich.rawdumper.ui.ModesInterface;
 import com.fkeglevich.rawdumper.ui.activity.FullscreenManager;
-import com.fkeglevich.rawdumper.util.Nothing;
-import com.fkeglevich.rawdumper.util.event.EventListener;
-import com.fkeglevich.rawdumper.util.exception.MessageException;
-import com.lantouzi.wheelview.WheelView;
 
 /**
  * Created by Flávio Keglevich on 29/08/2017.
@@ -55,77 +37,14 @@ public class MainActivity5 extends ModularActivity
     private MandatoryPermissionModule permissionModule = new MandatoryPermissionModule(reference);//new MandatoryRootModule(reference);
 
     private ModesInterface modesInterface;
-    private CameraManager cameraManager;
-    private CameraPreviewTexture textureView;
-
-    private ImageButton switchButton;
-
-    private EventListener<Nothing> pauseListener = new EventListener<Nothing>() {
-        @Override
-        public void onEvent(Nothing eventData)
-        {
-            cameraManager.closeCamera();
-            textureView.setAlpha(0);
-        }
-    };
-
-    private FeatureControllerManager featureControllerManager = new FeatureControllerManager();
+    private CameraLifetimeController cameraLifetimeController;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         getWindow().setBackgroundDrawable(null);
         setContentView(R.layout.activity_main);
-
         modesInterface = new ModesInterface(reference);
-        switchButton = (ImageButton) findViewById(R.id.camSwitchButton);
-        switchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textureView.startCloseCameraAnimation();
-                cameraManager.switchCamera();
-            }
-        });
-
-        featureControllerManager.createControllers(reference);
-
-        textureView = (CameraPreviewTexture) findViewById(R.id.textureView);
-
-        cameraManager = new CameraManager(reference, textureView);
-        cameraManager.onCameraOpened.addListener(new EventListener<TurboCamera>() {
-            @Override
-            public void onEvent(TurboCamera eventData)
-            {
-                init(eventData);
-            }
-        });
-
-        cameraManager.onCameraException.addListener(new EventListener<MessageException>() {
-            @Override
-            public void onEvent(MessageException eventData)
-            {
-                Log.i("CameraSetup", "ON EXCEPTION: " + eventData.getMessageResource(getApplicationContext()));
-            }
-        });
-
-        reference.onResume.addListener(new EventListener<Nothing>()
-        {
-            @Override
-            public void onEvent(Nothing eventData)
-            {
-                cameraManager.openCamera();
-            }
-        });
-    }
-
-    private void init(final TurboCamera turboCamera)
-    {
-        reference.onPause.removeListener(pauseListener);
-        reference.onPause.addListener(pauseListener);
-
-        textureView.setupPreview(turboCamera);
-        textureView.startOpenCameraAnimation();
-
-        featureControllerManager.setupControllers(turboCamera, cameraManager.onCameraClosed);
+        cameraLifetimeController = new CameraLifetimeController(reference);
     }
 }
