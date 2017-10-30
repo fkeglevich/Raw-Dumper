e62900ade3fe1dc29ac562fc465cc1ac314b565a
Flávio Keglevich <fkeglevich@gmail.com>
2017-10-30 19:04:44 -0200

    Added the picture pipelines
    A picture pipeline encapsulates the action of taking a picture and storing it

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/async/pipeline/PictureListener.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.camera.async.pipeline;
+
+/**
+ * TODO: Add class header
+ * <p>
+ * Created by Flávio Keglevich on 30/10/17.
+ */
+
+public interface PictureListener
+{
+    void onPictureTaken(boolean success);
+}
