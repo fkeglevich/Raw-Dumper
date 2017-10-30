3ac73c25c2326bc838ce300858bd1f68f36b2d3d
Flávio Keglevich <fkeglevich@gmail.com>
2017-10-30 19:08:24 -0200

    Created PictureFormat

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/data/PictureFormat.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.camera.data;
+
+import com.fkeglevich.rawdumper.camera.async.pipeline.NoPipeline;
+import com.fkeglevich.rawdumper.camera.async.pipeline.PicturePipeline;
+
+import static com.fkeglevich.rawdumper.camera.extension.IntelParameters.RAW_DATA_FORMAT_BAYER;
+import static com.fkeglevich.rawdumper.camera.extension.IntelParameters.RAW_DATA_FORMAT_NONE;
+
+/**
+ * TODO: Add class header
+ * <p>
+ * Created by Flávio Keglevich on 30/10/17.
+ */
+
+public enum PictureFormat implements ParameterValue
+{
+    JPEG(RAW_DATA_FORMAT_NONE, new NoPipeline()),
+    RAW(RAW_DATA_FORMAT_BAYER, new NoPipeline()),
+    YUV(RAW_DATA_FORMAT_NONE, new NoPipeline());
+
+    private final String rawDataFormatValue;
+    private final PicturePipeline picturePipeline;
+
+    PictureFormat(String rawDataFormatValue, PicturePipeline picturePipeline)
+    {
+        this.rawDataFormatValue = rawDataFormatValue;
+        this.picturePipeline = picturePipeline;
+    }
+
+    @Override
+    public String getParameterValue()
+    {
+        return rawDataFormatValue;
+    }
+
+    public PicturePipeline getPicturePipeline()
+    {
+        return picturePipeline;
+    }
+}
