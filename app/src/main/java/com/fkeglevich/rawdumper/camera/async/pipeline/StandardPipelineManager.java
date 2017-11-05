75490b28468b2d560fac949a18b1f185ec986623
Flávio Keglevich <fkeglevich@gmail.com>
2017-11-04 23:17:27 -0200

    Created StandardPipelineManager

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/async/pipeline/StandardPipelineManager.java
@@ -0,0 +1,11 @@
+package com.fkeglevich.rawdumper.camera.async.pipeline;
+
+/**
+ * TODO: Add class header
+ * <p>
+ * Created by Flávio Keglevich on 04/11/17.
+ */
+
+public class StandardPipelineManager
+{
+}
