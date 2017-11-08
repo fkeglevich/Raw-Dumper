73c057f2d0d302d29c271e98f4f9ad3397e8a3dd
Flávio Keglevich <fkeglevich@gmail.com>
2017-11-08 11:48:04 -0200

    Inits choosing raw format by default

--- app/src/main/java/com/fkeglevich/rawdumper/camera/async/impl/TurboCameraImpl.java
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/async/impl/TurboCameraImpl.java
@@ -92,6 +92,7 @@ public class TurboCameraImpl implements TurboCamera, Closeable
         createRestrictions();
         //mode format
         pictureModeFeature.setValue(pictureModeFeature.getAvailableValues().get(0));
+        pictureFormatFeature.setValue(pictureFormatFeature.getAvailableValues().get(1));
 
         ThreadUtil.simpleDelay(150);
     }
