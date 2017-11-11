74764543933b569745133f35cf2b9db4317dc235
Flávio Keglevich <fkeglevich@gmail.com>
2017-11-11 20:55:07 -0200

    Added clamp methods

--- app/src/main/java/com/fkeglevich/rawdumper/util/MathUtil.java
+++ app/src/main/java/com/fkeglevich/rawdumper/util/MathUtil.java
@@ -64,4 +64,25 @@ public class MathUtil
 
         return result;
     }
+
+    public static int clamp(int value, int min, int max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+
+    public static float clamp(float value, float min, float max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
+
+    public static double clamp(double value, double min, double max)
+    {
+        if (value < min) return min;
+        if (value > max) return max;
+        return value;
+    }
 }
