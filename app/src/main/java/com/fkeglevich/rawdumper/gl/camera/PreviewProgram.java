ed1ce1d8d5558145d4649f56e7e0acfaca470d3f
Unknown <fkeglevich@gmail.com>
2017-12-11 13:34:50 -0200

    Start caching values of preview programs

--- app/src/main/java/com/fkeglevich/rawdumper/gl/camera/PreviewProgram.java
+++ app/src/main/java/com/fkeglevich/rawdumper/gl/camera/PreviewProgram.java
@@ -23,6 +23,7 @@ import com.fkeglevich.rawdumper.gl.exception.GLException;
 import com.fkeglevich.rawdumper.gl.exception.GLUncheckedException;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * Created by flavio on 26/11/17.
@@ -30,6 +31,10 @@ import java.nio.ByteBuffer;
 
 public class PreviewProgram extends Program
 {
+    private float[] surfaceMatrixCache = new float[16];
+    private float[] surfaceSizeCache   = new float[2];
+    private float[] previewScaleCache  = new float[2];
+
     public static PreviewProgram create() throws GLException
     {
         return new PreviewProgram(Program.create().getHandle());
@@ -40,30 +45,45 @@ public class PreviewProgram extends Program
         return new PreviewProgram(program.getHandle());
     }
 
-    private PreviewProgram(int handle)
+    PreviewProgram(int handle)
     {
         super(handle);
     }
 
     void setSurfaceMatrix(float[] surfaceMatrix)
     {
+        if (Arrays.equals(surfaceMatrix, surfaceMatrixCache)) return;
+
         int handle = getUniformHandle("surfaceMatrix");
         if (handle != -1)
+        {
             GLES20.glUniformMatrix4fv(handle, 1, false, surfaceMatrix, 0);
+            System.arraycopy(surfaceMatrix, 0, surfaceMatrixCache, 0, surfaceMatrix.length);
+        }
     }
 
     void setSurfaceSize(float[] surfaceSize)
     {
+        if (Arrays.equals(surfaceSize, surfaceSizeCache)) return;
+
         int handle = getUniformHandle("surfaceSize");
         if (handle != -1)
+        {
             GLES20.glUniform2fv(handle, 1, surfaceSize, 0);
+            System.arraycopy(surfaceSize, 0, surfaceSizeCache, 0, surfaceSize.length);
+        }
     }
 
     void setPreviewScale(float[] previewScale)
     {
+        if (Arrays.equals(previewScale, previewScaleCache)) return;
+
         int handle = getUniformHandle("previewScale");
         if (handle != -1)
+        {
             GLES20.glUniform2fv(handle, 1, previewScale, 0);
+            System.arraycopy(previewScale, 0, previewScaleCache, 0, previewScale.length);
+        }
     }
 
     void setupVertices(ByteBuffer vertexBuffer)
