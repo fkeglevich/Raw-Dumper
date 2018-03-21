6ba0dd9556c747501208c9e21eec5e32cf57b8ee
Unknown <fkeglevich@gmail.com>
2018-03-21 17:02:40 -0300

    Created ScreenFlash enum

---
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/data/ScreenFlash.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018, Flávio Keglevich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fkeglevich.rawdumper.camera.data;
+
+import com.fkeglevich.rawdumper.raw.data.ExifFlash;
+
+/**
+ * Represents the possible values for an screen front camera flash.
+ *
+ * Created by Flávio Keglevich on 21/03/18.
+ */
+
+public enum ScreenFlash
+{
+    OFF(ExifFlash.DID_NOT_FIRE),
+    ON(ExifFlash.FIRED);
+
+    private final ExifFlash exifFlash;
+
+    ScreenFlash(ExifFlash exifFlash)
+    {
+        this.exifFlash = exifFlash;
+    }
+
+    public ExifFlash getExifFlash()
+    {
+        return exifFlash;
+    }
+}
