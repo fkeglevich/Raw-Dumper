fce6cf1c86889e04199099ba9daae46635543a70
Unknown <fkeglevich@gmail.com>
2018-03-25 19:27:39 -0300

    Added R, G and B positions
    For future algorithms that will process bayer data directly

--- app/src/main/java/com/fkeglevich/rawdumper/raw/data/BayerPattern.java
+++ app/src/main/java/com/fkeglevich/rawdumper/raw/data/BayerPattern.java
@@ -33,22 +33,50 @@ public enum BayerPattern
         GREEN = 1
         BLUE = 2
      */
-    BGGR(new byte[] {2, 1, 1, 0}),
-    RGGB(new byte[] {0, 1, 1, 2}),
-    GBRG(new byte[] {1, 2, 0, 1}),
-    GRBG(new byte[] {1, 0, 2, 1}),
-    UNKNOWN(null);
+    BGGR(new byte[] {2, 1, 1, 0}, 3, 0, 2, 1),
+    RGGB(new byte[] {0, 1, 1, 2}, 0, 3, 1, 2),
+    GBRG(new byte[] {1, 2, 0, 1}, 2, 1, 3, 0),
+    GRBG(new byte[] {1, 0, 2, 1}, 1, 2, 0, 3),
+    UNKNOWN(null, -1, -1, -1, -1);
 
     private final byte[] bytePattern;
+    private final int R;
+    private final int B;
+    private final int Gr;
+    private final int Gb;
 
-    BayerPattern(byte[] bytePattern)
+    BayerPattern(byte[] bytePattern, int R, int B, int Gr, int Gb)
     {
         this.bytePattern = bytePattern;
+        this.R = R;
+        this.B = B;
+        this.Gr = Gr;
+        this.Gb = Gb;
     }
 
     public byte[] getBytePattern()
     {
         if (bytePattern == null) throw new RuntimeException("An unknown bayer pattern can't be encoded!");
         return bytePattern.clone();
+    }
+
+    public int getR()
+    {
+        return R;
+    }
+
+    public int getB()
+    {
+        return B;
+    }
+
+    public int getGr()
+    {
+        return Gr;
+    }
+
+    public int getGb()
+    {
+        return Gb;
     }
 }
