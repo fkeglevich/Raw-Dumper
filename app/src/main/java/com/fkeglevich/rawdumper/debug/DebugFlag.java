b8559265871c224874c5050f729798ee25d86214
Unknown <fkeglevich@gmail.com>
2018-05-06 15:48:37 -0300

    Disabled debug flag

--- app/src/main/java/com/fkeglevich/rawdumper/debug/DebugFlag.java
+++ app/src/main/java/com/fkeglevich/rawdumper/debug/DebugFlag.java
@@ -32,7 +32,7 @@ public class DebugFlag
     private static final DebugFlag OPEN_FRONT_CAMERA_FIRST  = new DebugFlag(false, false);
     private static final DebugFlag RETRY_PIPELINE_SIMULATOR = new DebugFlag(false, false);
     private static final DebugFlag FORCE_RETRY_PIPELINE     = new DebugFlag(false, false);
-    private static final DebugFlag FORCE_RAW_ZENFONE_ZOOM   = new DebugFlag(!false, false);
+    private static final DebugFlag FORCE_RAW_ZENFONE_ZOOM   = new DebugFlag(false, false);
     private static final DebugFlag DISABLE_LOGCAT_SERVICE   = new DebugFlag(false, false);
 
     private final boolean value;
