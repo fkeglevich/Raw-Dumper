37f7d01a23b1c1f0b56a7394af4e264ce8b2014b
Unknown <fkeglevich@gmail.com>
2018-05-09 20:45:06 -0300

    Commented out parameters due to name inconsistensies

--- app/src/main/java/com/fkeglevich/rawdumper/camera/extension/AsusParameters.java
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/extension/AsusParameters.java
@@ -39,10 +39,10 @@ public class AsusParameters
 
     public static final String ASUS_IMAGE_OPTIMIZE = "image_optimize";
 
-    public static final Parameter<ManualFocus> MANUAL_FOCUS = ParameterFactory.create("set_manual_focus", ManualFocus.class);
+    public static final Parameter<ManualFocus> MANUAL_FOCUS = ParameterFactory.createAsyncEvent("set_manual_focus", ManualFocus.class);
     public static final Parameter<ManualFocusRange> MANUAL_FOCUS_RANGE = ParameterFactory.createReadOnly("focus_range_values", ManualFocusRange.class);
 
-    public static final Parameter<ManualTemperature> MANUAL_TEMPERATURE = ParameterFactory.create("asus_color_temp", ManualTemperature.class);
+    public static final Parameter<ManualTemperature> MANUAL_TEMPERATURE = ParameterFactory.createAsyncEvent("asus_color_temp", ManualTemperature.class);
     //public static final Parameter<ManualTemperatureRange> MANUAL_TEMPERATURE_RANGE = ParameterFactory.createReadOnly("color_temp_values", ManualTemperatureRange.class);
-    public static final Parameter<ManualTemperatureRange> MANUAL_TEMPERATURE_RANGE = ParameterFactory.createReadOnly("preview_color_temp", ManualTemperatureRange.class);
+    //public static final Parameter<ManualTemperatureRange> MANUAL_TEMPERATURE_RANGE = ParameterFactory.createReadOnly("preview_color_temp", ManualTemperatureRange.class);
 }
