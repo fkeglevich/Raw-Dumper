8289158e39485e30913db87c16a9b682aa49d1d0
Unknown <fkeglevich@gmail.com>
2018-05-16 03:19:04 -0300

    Removed useless comments

--- app/src/main/java/com/fkeglevich/rawdumper/camera/extension/AsusParameters.java
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/extension/AsusParameters.java
@@ -42,6 +42,4 @@ public class AsusParameters
     public static final Parameter<ManualFocusRange> MANUAL_FOCUS_RANGE = ParameterFactory.createReadOnly("focus_range_values", ManualFocusRange.class);
 
     public static final Parameter<ManualTemperature> MANUAL_TEMPERATURE = ParameterFactory.createAsyncEvent("asus_color_temp", ManualTemperature.class);
-    //public static final Parameter<ManualTemperatureRange> MANUAL_TEMPERATURE_RANGE = ParameterFactory.createReadOnly("color_temp_values", ManualTemperatureRange.class);
-    //public static final Parameter<ManualTemperatureRange> MANUAL_TEMPERATURE_RANGE = ParameterFactory.createReadOnly("preview_color_temp", ManualTemperatureRange.class);
 }
