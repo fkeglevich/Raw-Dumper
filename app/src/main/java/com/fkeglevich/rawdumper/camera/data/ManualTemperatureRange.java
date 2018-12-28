c2f52a98313e716f0898c084b9683d8f334f4f5a
Unknown <fkeglevich@gmail.com>
2018-12-28 15:14:25 -0200

    Simplified assertion

--- app/src/main/java/com/fkeglevich/rawdumper/camera/data/ManualTemperatureRange.java
+++ app/src/main/java/com/fkeglevich/rawdumper/camera/data/ManualTemperatureRange.java
@@ -42,7 +42,7 @@ public class ManualTemperatureRange extends DataRange<ManualTemperature>
         int[] temperatureRange = colorInfo.getTemperatureRange();
         if (temperatureRange == null) return null;
 
-        Assert.assertTrue(temperatureRange.length == 4);
+        Assert.assertEquals(4, temperatureRange.length);
 
         ManualTemperature lower = ManualTemperature.create(temperatureRange[0]);
         ManualTemperature upper = ManualTemperature.create(temperatureRange[temperatureRange.length - 1]);
